54f7b12839d7a8dea9927a003b6c7ad1638da609
henryAs <henry.albiri@renos.id>
2025-03-25 16:19:32 +0700

    feat: [update] update

--- src/containers/desktop/home/Components/submit/index.tsx
+++ src/containers/desktop/home/Components/submit/index.tsx
@@ -1,10 +1,18 @@
 "use client";
 import useTranslation from "@app/src/app/Dictionaries/hooks/useTranslation";
+import { HomeContext } from "@app/src/modules/home/contexts";
 import { Box, Button } from "@mui/material";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useContext } from "react";
 
 const Submit = () => {
   const { t } = useTranslation("common");
+  const { city, date, countOfVisitor } = useContext(HomeContext);
+  const { push } = useRouter();
+  const findHotel = () => {
+    push(`/search?city=${city}&date=${date}&countOfVisitor=${countOfVisitor}`);
+  };
+
   return (
     <Box
       sx={{
@@ -21,6 +29,7 @@ const Submit = () => {
         color="primary"
         sx={{ borderRadius: 18 }}
         size="large"
+        onClick={() => findHotel()}
       >
         {t("find-hotel")}
       </Button>
